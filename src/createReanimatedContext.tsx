ebd77fc014cedace2108ea4c351be0e7a1bdf49f
Mike Bybee <me@mikebybee.com>
2023-02-19 20:31:03 -0600

    fix: :label: Fix types

--- src/createReanimatedContext.tsx
+++ src/createReanimatedContext.tsx
@@ -4,61 +4,73 @@ import React, {
   useContext,
   useRef,
   useSyncExternalStore,
-} from "react";
-import { useSharedValue } from "react-native-reanimated";
+} from 'react'
+import { useSharedValue } from 'react-native-reanimated'
 
-export function createReanimatedContext<Store>(initialData: Store) {
+interface ProviderProps<Store> {
+  children: React.ReactNode
+  value: Store
+}
+
+export function createReanimatedContext<Store>(
+  initialData: Store,
+): [
+  Provider: React.ComponentType<ProviderProps<Store>>,
+  useFastContext: <SelectorOutput>(
+    selector: (store: Store) => SelectorOutput,
+  ) => [SelectorOutput, (value: Partial<Store>) => void],
+] {
   function useReanimatedData(initialData: Store): {
-    get: () => Store;
-    set: (value: Partial<Store>) => void;
-    subscribe: (callback: () => void) => () => void;
+    get: () => Store
+    set: (value: Partial<Store>) => void
+    subscribe: (callback: () => void) => () => void
   } {
-    const store = useSharedValue(initialData);
+    const store = useSharedValue(initialData)
 
-    const get = useCallback(() => store.value, []);
+    const get = useCallback(() => store.value, [])
 
-    const subscribers = useRef(new Set<() => void>());
+    const subscribers = useRef(new Set<() => void>())
 
     const set = useCallback((value: Partial<Store>) => {
-      store.value = { ...store.value, ...value };
-      subscribers.current.forEach((callback) => callback());
-    }, []);
+      store.value = { ...store.value, ...value }
+      subscribers.current.forEach((callback) => callback())
+    }, [])
 
     const subscribe = useCallback((callback: () => void) => {
-      subscribers.current.add(callback);
-      return () => subscribers.current.delete(callback);
-    }, []);
+      subscribers.current.add(callback)
+      return () => subscribers.current.delete(callback)
+    }, [])
 
     return {
       get,
       set,
       subscribe,
-    };
+    }
   }
 
-  type UseStoreDataReturnType = ReturnType<typeof useReanimatedData>;
+  type UseStoreDataReturnType = ReturnType<typeof useReanimatedData>
 
-  const Context = createContext<UseStoreDataReturnType | null>(null);
+  const Context = createContext<UseStoreDataReturnType | null>(null)
   function Provider({ children }: { children: React.ReactNode }) {
     return (
       <Context.Provider value={useReanimatedData(initialData)}>
         {children}
       </Context.Provider>
-    );
+    )
   }
   function useReanimatedContext<SelectorOutput>(
-    selector: (store: Store) => SelectorOutput
+    selector: (store: Store) => SelectorOutput,
   ): [SelectorOutput, (value: Partial<Store>) => void] {
-    const store = useContext(Context);
+    const store = useContext(Context)
     if (!store) {
-      throw new Error("Store not found");
+      throw new Error('Store not found')
     }
 
     const state = useSyncExternalStore(store.subscribe, () =>
-      selector(store.get())
-    );
+      selector(store.get()),
+    )
 
-    return [state, store.set];
+    return [state, store.set]
   }
-  return [Provider, useReanimatedContext];
+  return [Provider, useReanimatedContext]
 }
--- src/createRefContext.tsx
+++ src/createRefContext.tsx
@@ -4,61 +4,73 @@ import React, {
   useContext,
   useRef,
   useSyncExternalStore,
-} from "react";
+} from 'react'
 
-export function createRefContext<Store>(initialData: Store) {
+interface ProviderProps<Store> {
+  children: React.ReactNode
+  value: Store
+}
+
+export function createRefContext<Store>(
+  initialData: Store,
+): [
+  Provider: React.ComponentType<ProviderProps<Store>>,
+  useFastContext: <SelectorOutput>(
+    selector: (store: Store) => SelectorOutput,
+  ) => [SelectorOutput, (value: Partial<Store>) => void],
+] {
   function useStoreData(initialData: Store): {
-    get: () => Store;
-    set: (value: Partial<Store>) => void;
-    subscribe: (callback: () => void) => () => void;
+    get: () => Store
+    set: (value: Partial<Store>) => void
+    subscribe: (callback: () => void) => () => void
   } {
-    const store = useRef(initialData);
+    const store = useRef(initialData)
 
-    const get = useCallback(() => store.current, []);
+    const get = useCallback(() => store.current, [])
 
-    const subscribers = useRef(new Set<() => void>());
+    const subscribers = useRef(new Set<() => void>())
 
     const set = useCallback((value: Partial<Store>) => {
-      store.current = { ...store.current, ...value };
-      subscribers.current.forEach((callback) => callback());
-    }, []);
+      store.current = { ...store.current, ...value }
+      subscribers.current.forEach((callback) => callback())
+    }, [])
 
     const subscribe = useCallback((callback: () => void) => {
-      subscribers.current.add(callback);
-      return () => subscribers.current.delete(callback);
-    }, []);
+      subscribers.current.add(callback)
+      return () => subscribers.current.delete(callback)
+    }, [])
 
     return {
       get,
       set,
       subscribe,
-    };
+    }
   }
 
-  type UseStoreDataReturnType = ReturnType<typeof useStoreData>;
+  type UseStoreDataReturnType = ReturnType<typeof useStoreData>
 
-  const Context = createContext<UseStoreDataReturnType | null>(null);
+  const Context = createContext<UseStoreDataReturnType | null>(null)
   function Provider({ children }: { children: React.ReactNode }) {
     return (
       <Context.Provider value={useStoreData(initialData)}>
         {children}
       </Context.Provider>
-    );
+    )
   }
 
   function useFastContext<SelectorOutput>(
-    selector: (store: Store) => SelectorOutput
+    selector: (store: Store) => SelectorOutput,
   ): [SelectorOutput, (value: Partial<Store>) => void] {
-    const store = useContext(Context);
+    const store = useContext(Context)
     if (!store) {
-      throw new Error("Store not found");
+      throw new Error('Store not found')
     }
 
     const state = useSyncExternalStore(store.subscribe, () =>
-      selector(store.get())
-    );
+      selector(store.get()),
+    )
 
-    return [state, store.set];
+    return [state, store.set]
   }
-  return [Provider, useFastContext];
+  return [Provider, useFastContext]
 }
